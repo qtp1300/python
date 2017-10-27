516ad270f997248474c9a2054bb67126d994754e
祁天培 <qtp1300@163.com>
2017-10-27 18:34:26 +0800

    file IO

--- py2_pycharm/message_boom/message boom.py
+++ py2_pycharm/message_boom/message boom.py
@@ -1,5 +1,14 @@
 import urllib2
-url = "http://www.baidu.com"
-wangye = urllib2.urlopen(url)
-open()
-print(wangye.read())
+# from sys import argv
+# url = "http://www.baidu.com"
+# wangye = urllib2.urlopen(url)
+txtfile = open("temp.txt",'r')
+neirong = txtfile.read()
+print (neirong)
+txtfile.close()
+txtfile = open("temp.txt",'w')
+strr = "3333"
+# txtfile.writable()
+txtfile.write(strr)
+txtfile.close()
+# print(wangye.read())
