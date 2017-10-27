586102d301d30de033dd21d77e2492bcc4e8e21b
祁天培 <qtp1300@163.com>
2017-10-27 22:10:22 +0800

    得到登陆网页，正在写post请求 网上的post请求格式，怀疑不应在第一次打开网页是发送请求数据

--- py2_pycharm/message_boom/message boom.py
+++ py2_pycharm/message_boom/message boom.py
@@ -9,7 +9,7 @@ def jump():
     nr = nr[32:]
     # print nr
     nr = nr[:-30]
-    print nr
+    # print nr
     txtfile = open("temp.txt", 'w')
     txtfile.write(nr)
     txtfile.close()
@@ -26,7 +26,21 @@ def loginpage():
     temp2.close()
 
 def post():
-    postn = urllib.urlencode({'operatorPwd': '','operatorUserId':'','password':'1234561','queryString':'wlanuserip%3D10.55.6.184%26wlanacname%3DZZTZ_HX_N18007%26ssid%3D%26nasip%3D10.54.2.13%26snmpagentip%3D%26mac%3Dca4f43cd3f9d%26t%3Dwireless-v2-plain%26url%3Dhttp%3A%2F%2Fnewtab.firefoxchina.cn%2Ferror-tab-rec.html%26apmac%3D%26nasid%3DZZTZ_HX_N18007%26vid%3D4022%26port%3D57%26nasportid%3DAggregatePort%25201.40220000%3A4022-0','service':'','userId':'150243003','validcode':''})
-    # postn = urllib.urlencode({'qq':''})
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = {'User-Agent': user_agent}
+    xuehao = '150243003'
+    mima = '1234561'
+    values = urllib.urlencode({'operatorPwd': '','operatorUserId':'','password':mima,'queryString':'wlanuserip%3D10.55.6.184%26wlanacname%3DZZTZ_HX_N18007%26ssid%3D%26nasip%3D10.54.2.13%26snmpagentip%3D%26mac%3Dca4f43cd3f9d%26t%3Dwireless-v2-plain%26url%3Dhttp%3A%2F%2Fnewtab.firefoxchina.cn%2Ferror-tab-rec.html%26apmac%3D%26nasid%3DZZTZ_HX_N18007%26vid%3D4022%26port%3D57%26nasportid%3DAggregatePort%25201.40220000%3A4022-0','service':'','userId':xuehao,'validcode':''})
+
+
+    req = urllib2.Request(open('temp.txt','r').read(), values, headers)
+    # aaa = 111
+    response = urllib2.urlopen(req)
+    jieguo = response.read()
+    print jieguo
+    temp3 = open('temp3.html','w')
+    temp3.write(jieguo)
+
 jump()
 loginpage()
+post()
